4b85cb471a56a9cb64575d9981a0bbfd3a6a4e30
Kelvin Jiang <k.jiang1118@gmail.com>
2018-07-01 20:05:20 -0400

    provided a seed so that cross-validation would always generate training and testing sets in the same way

--- cross_validation.py
+++ cross_validation.py
@@ -14,15 +14,8 @@ X = stats_df.drop(['playerID', 'yearID', 'all_star?'], axis=1)
 y = stats_df['all_star?']
 
 # cross-validation
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 logging.info('SUCCESSFULLY PREPARED TRAINING AND TESTING SETS')
-
-# copy all-star data in training set
-copies = 5
-train = pd.concat([X_train, y_train], axis=1)
-train = train.append([train[train['all_star?'] == 1]] * copies, ignore_index=True)
-X_train = train.drop(['all_star?'], axis=1)
-y_train = train['all_star?']
 
 # save data to pickle
 with open('preprocessed/stats_train_test.pickle', 'wb') as f:
--- data_visualization.py
+++ data_visualization.py
@@ -2,27 +2,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # parameters
-x_stat = 'yearID'
-x_stat_name = 'Year'
+x_stat = 'H'
+x_stat_name = 'Hits'
 y_stat = 'AVG'
-y_stat_name = 'Season Batting Average'
-games_threshold = 100
+y_stat_name = 'Batting Average'
+games_threshold = 20
 
 # read in data from CSV
 stats_df = pd.read_csv('preprocessed/stats.csv')
+
+pd.set_option('display.column_space', 100)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.width', 500)
 
 # initialize figure and subplot
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-# batting average vs year (more than 100 G)
-non_all_stars = stats_df[(stats_df['all_star?'] == 0) & (stats_df['G'] > games_threshold)]
-all_stars = stats_df[(stats_df['all_star?'] == 1) & (stats_df['G'] > games_threshold)]
+# batting average vs year
+stats_df = stats_df[stats_df['G'] > games_threshold]
+non_all_stars = stats_df[stats_df['all_star?'] == 0]
+all_stars = stats_df[stats_df['all_star?'] == 1]
 ax.scatter(non_all_stars[x_stat], non_all_stars[y_stat], s=1, c='r', label='Non All-Stars')
 ax.scatter(all_stars[x_stat], all_stars[y_stat], s=1, c='b', label='All-Stars')
 ax.set_xlabel(x_stat_name)
 ax.set_ylabel(y_stat_name)
-ax.set_title('%s vs. %s' % (x_stat_name, y_stat_name))
-ax.legend(loc='lower right', scatterpoints=1)
+ax.set_title('Season %s vs. %s' % (x_stat_name, y_stat_name))
+ax.legend(loc='best', scatterpoints=1)
 
 plt.show()
