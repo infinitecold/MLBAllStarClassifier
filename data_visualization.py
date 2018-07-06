aafd429a85d180ddb5e8e6f50e71012ee492cb28
Kelvin Jiang <k.jiang1118@gmail.com>
2018-07-05 20:14:59 -0400

    configured data visualization script to create either histogram or scatterplot

--- data_visualization.py
+++ data_visualization.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # parameters
+graph_type = 'histogram'  # 'histogram' or 'scatterplot'
 x_stat = 'H'
 x_stat_name = 'Hits'
 y_stat = 'AVG'
@@ -20,15 +21,24 @@ pd.set_option('display.width', 500)
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-# batting average vs year
-stats_df = stats_df[stats_df['G'] > games_threshold]
-non_all_stars = stats_df[stats_df['all_star?'] == 0]
-all_stars = stats_df[stats_df['all_star?'] == 1]
-ax.scatter(non_all_stars[x_stat], non_all_stars[y_stat], s=1, c='r', label='Non All-Stars')
-ax.scatter(all_stars[x_stat], all_stars[y_stat], s=1, c='b', label='All-Stars')
-ax.set_xlabel(x_stat_name)
-ax.set_ylabel(y_stat_name)
-ax.set_title('Season %s vs. %s' % (x_stat_name, y_stat_name))
-ax.legend(loc='best', scatterpoints=1)
+def histogram(ax):
+    ax.hist(stats_df[x_stat], bins=250)
+    ax.set_xlabel(x_stat_name)
+    ax.set_ylabel('Frequency')
+    ax.set_title('Season %s Distribution' % x_stat_name)
+    return ax
 
+def scatterplot(ax):
+    non_all_stars = stats_df[stats_df['all_star?'] == 0]
+    all_stars = stats_df[stats_df['all_star?'] == 1]
+    ax.scatter(non_all_stars[x_stat], non_all_stars[y_stat], s=1, c='r', label='Non All-Stars')
+    ax.scatter(all_stars[x_stat], all_stars[y_stat], s=1, c='b', label='All-Stars')
+    ax.set_xlabel(x_stat_name)
+    ax.set_ylabel(y_stat_name)
+    ax.set_title('Season %s vs. %s' % (x_stat_name, y_stat_name))
+    ax.legend(loc='best', scatterpoints=1)
+    return ax
+
+stats_df = stats_df[stats_df['G'] > games_threshold]
+ax = histogram(ax) if graph_type == 'histogram' else scatterplot(ax)
 plt.show()
