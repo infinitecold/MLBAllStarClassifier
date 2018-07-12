271a035bc111c449131433a19f82b98df69cba2b
Kelvin Jiang <k.jiang1118@gmail.com>
2018-07-11 20:22:59 -0400

    changed cross-validation to split train and test sets based on year

--- cross_validation.py
+++ cross_validation.py
@@ -1,7 +1,6 @@
 import logging
 import pandas as pd
 import pickle
-from sklearn.model_selection import train_test_split
 
 # package options
 logging.basicConfig(format='%(asctime)s %(levelname)s\t%(message)s', level=logging.INFO)
@@ -10,13 +9,27 @@ logging.basicConfig(format='%(asctime)s %(levelname)s\t%(message)s', level=loggi
 stats_df = pd.read_csv('data/preproc/stats.csv')
 logging.info('SUCCESSFULLY LOADED CSV(\'s) INTO DATAFRAME(\'s)')
 
-# organize features (X) and labels (y)
-X = stats_df.drop(['playerID', 'yearID', 'all_star?'], axis=1)
-y = stats_df['all_star?']
+# cross-validation by year
+train_df = stats_df[stats_df['yearID'] < 2010].copy()
+test_df = stats_df[stats_df['yearID'] >= 2010].copy()
 
-# cross-validation
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-logging.info('SUCCESSFULLY PREPARED TRAINING AND TESTING SETS')
+# fill missing data with all-star/non-all-star averages from train set
+columns_missing_data = train_df.columns[train_df.isnull().any()].tolist()
+logging.info('COLUMNS MISSING DATA: %s', columns_missing_data)
+for column in columns_missing_data:
+    allstar_avg = train_df[train_df['all_star?'] == 1][column].mean()
+    train_df.loc[(train_df[column].isnull()) & (train_df['all_star?'] == 1), [column]] = allstar_avg
+    test_df.loc[(test_df[column].isnull()) & (test_df['all_star?'] == 1), [column]] = allstar_avg
+    non_allstar_avg = train_df[train_df['all_star?'] == 0][column].mean()
+    train_df.loc[(train_df[column].isnull()) & (train_df['all_star?'] == 0), [column]] = non_allstar_avg
+    test_df.loc[(test_df[column].isnull()) & (test_df['all_star?'] == 0), [column]] = non_allstar_avg
+    logging.info('TRAIN SET COLUMN %s AVERAGES: ALL-STAR %f NON-ALL-STAR %f', column, allstar_avg, non_allstar_avg)
+
+# organize features (X) and labels (y)
+X_train = train_df.drop(['all_star?'], axis=1)
+X_test = test_df.drop(['all_star?'], axis=1)
+y_train = train_df['all_star?']
+y_test = test_df['all_star?']
 
 # save data to pickle
 with open('data/preproc/stats_cv.pickle', 'wb') as f:
